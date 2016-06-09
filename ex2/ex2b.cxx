d86b2586f81df6334ffddb8b16587ca68fcebf6f
pletzera <pletzera@niwa-1007520.niwa.local>
2016-06-10 11:18:37 +1200

    adding cmake build

--- ex2/ex2b.cxx
+++ ex2/ex2b.cxx
@@ -1,3 +1,4 @@
+
 #include <omp.h>
 #include <cstdlib>
 #include <iostream>
@@ -5,31 +6,40 @@
 // compute pi from the integral 4/(1 + x^2) from 0 to 1
 
 int main(int argc, char** argv) {
+
+    if (argc < 2) {
+      std::cerr << "Need number of steps\n";
+      std::cerr << argv[0] << " <numSteps>\n";
+      std::exit(1);
+    }
     int numSteps = atoi(argv[1]);
     double dx = 1.0 / numSteps;
-    double sum = 0;
-    int nth = 0;
+    int numThreads = 0;
     
 #pragma omp parallel
 {
-    int threadId = omp_get_thread_num();
-    int numThreads = omp_get_num_threads();
-    if (threadId == 0) {
-        nth = numThreads;
+    int id = omp_get_thread_num();
+    int nth = omp_get_num_threads();
+    if (id == 0) {
+        // store the number of threads in shared variable
+        numThreads = nth;
     }
 }
-    std::cout << "number of threads: " << nth << '\n';
-    double partialSums[nth];
+    std::cout << "number of threads: " << numThreads << '\n';
+    double partialSums[numThreads];
+    double sum = 0.0;
 #pragma omp parallel
 {
-    int threadId = omp_get_thread_num();
-    partialSums[threadId] = 0;
-    for (int i = threadId; i < numSteps; i += nth) {
+    int id = omp_get_thread_num();
+    partialSums[id] = 0;
+    for (int i = id; i < numSteps; i += numThreads) {
         double x = (i + 0.5)*dx;
-        partialSums[threadId] += dx * 4. / (1. + x*x);
+        partialSums[id] += dx * 4. / (1. + x*x);
     }
 #pragma omp atomic 
-    sum += partialSums[threadId];
+    sum += partialSums[id];
 }
     std::cout << "approx pi = " << sum << " error = " << M_PI - sum << '\n';
+
+    return 0;
 }
